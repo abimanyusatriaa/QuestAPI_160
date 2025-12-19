f5b1df372961ec8a11770457378c62f127920986
abimanyusatriaa <adji3890@gmail.com>
2025-12-19 20:16:53 +0700

    menambah package route di dalam uicontroller dan menambah file destinasi home

---
+++ app/src/main/java/com/example/local/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.local.uicontroller.route
+
+import com.example.local.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
