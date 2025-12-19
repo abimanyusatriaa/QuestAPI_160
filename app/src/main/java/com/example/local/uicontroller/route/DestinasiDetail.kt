3d49fdb2123d1df499da61b170bbc95dfaa2fe1a
abimanyusatriaa <adji3890@gmail.com>
2025-12-19 20:16:35 +0700

    menambah package route di dalam uicontroller dan menambah file destinasi detail

---
+++ app/src/main/java/com/example/local/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.local.uicontroller.route
+
+import com.example.local.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
