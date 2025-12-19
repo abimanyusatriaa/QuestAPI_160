06d0fa5f0660fe295d2441c735e760b759ea0a22
abimanyusatriaa <adji3890@gmail.com>
2025-12-19 20:14:59 +0700

    menambah file repository data siswa

---
+++ app/src/main/java/com/example/local/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.local.repositori
+
+import com.example.local.apiservice.ServiceApiSiswa
+import com.example.local.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
