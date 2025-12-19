4d64a343769f436efdacc3d9a19d5fbec83eaa71
abimanyusatriaa <adji3890@gmail.com>
2025-12-19 20:21:12 +0700

    memanggil di main

--- app/src/main/java/com/example/local/MainActivity.kt
+++ app/src/main/java/com/example/local/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.local.ui.theme.LocalTheme
+import com.example.local.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocalTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
