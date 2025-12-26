14fe6a437cde3e535c37eeab64a1e4d70e2fb1c9
abimanyusatriaa <adji3890@gmail.com>
2025-12-26 20:01:22 +0700

    menambah function get satu siswa

--- app/src/main/java/com/example/local/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/local/repositori/RepositoryDataSiswa.kt
@@ -3,15 +3,20 @@ package com.example.local.repositori
 import com.example.local.apiservice.ServiceApiSiswa
 import com.example.local.modeldata.DataSiswa
 
+
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
-}
+    //edit: tambahkan fungsi getSatuSiswa
+    suspend fun getSatuSiswa(id: Int): DataSiswa
 
+
+}
 class jaringanRepositoryDataSiswa (
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+
 }
