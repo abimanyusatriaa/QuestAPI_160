10486cff8b09ccfdc139053fcc4c6017bde95773
abimanyusatriaa <adji3890@gmail.com>
2025-12-26 20:13:28 +0700

    menambah isi file halaman edit

--- app/src/main/java/com/example/local/view/HalamanEdit.kt
+++ app/src/main/java/com/example/local/view/HalamanEdit.kt
@@ -1,2 +1,23 @@
 package com.example.local.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.local.uicontroller.route.DestinasiEdit
+import com.example.local.viewmodel.EditViewModel
+import com.example.local.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
